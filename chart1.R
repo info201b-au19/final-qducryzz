b188cdafa135b57487587ab1c324857f4fc227ca
hongyi <hongyi@Stevens-MacBook-Pro.local>
2019-11-18 19:57:43 -0800

    haha

--- chart1.R
+++ chart1.R
@@ -3,6 +3,12 @@ library(plotly)
 library(tidyr)
 library(dplyr)
 
+# This chart was intended to show the relationship between
+# CO2 emmisions per capita and GDP per capita for
+# diffrent countries in the world from 1800-2016.
+# It is pretty obvious that the U.S. always in a 
+# very high position on both GDP per capita and 
+# CO2 emission per capita after 1900.
 
 co_emission_data <- read.csv("./data/GDP&CO2.csv")
 
@@ -12,7 +18,7 @@ co_emission_data_nw <- co_emission_data %>%
   filter(Year >= 1800 & Year <= 2016)
 View(co_emission_data_nw)
 ui <- fluidPage(
-  h1("CO2 emissions per capital vs GDP per capital"),
+  h1("CO2 emissions per capita vs GDP per capita"),
 
   sidebarLayout(
     sidebarPanel(
@@ -28,7 +34,10 @@ ui <- fluidPage(
     )
   )
 )
-
+# Since both China and India has very high population, I 
+# choose to calculate the parameter of those markers in
+# a slight different way so that they won't take over the whole 
+# chart.
 server <- function(input, output) {
   output$BubbleChart <- renderPlotly({
     co_emission_year <- co_emission_data_nw %>%
@@ -43,7 +52,7 @@ server <- function(input, output) {
         co_emission_year$Entity == "China" |
           co_emission_year$Entity == "India",
         "Total_population"
-      ] / 10
+      ] * 5 / 100
 
 
     plot_ly(co_emission_year,
@@ -52,14 +61,20 @@ server <- function(input, output) {
       color = co_emission_year$Entity, type = "scatter", mode = "markers",
       marker = list(
         symbol = "cirlce", sizemode = "diameter", opacity = 0.8,
-        size = co_emission_year$Total_population * 5 / 30000000
+        size = co_emission_year$Total_population * 5 / 20000000
       ),
       source = "plot",
       key = co_emission_year$Entity
     ) %>%
       layout(
-        xaxis = list(title = "GDP per capital"),
-        yaxis = list(title = "CO2 emissions per capital")
+        xaxis = list(title = "GDP per capita", range = c(
+          0,
+          max(co_emission_year$GDP_per_capita)
+        )),
+        yaxis = list(
+          title = "CO2 emissions per capita (in tons)",
+          range = c(0, max(co_emission_year$CO2_emissions_per_capita) + 5)
+        )
       )
   })
 
@@ -72,7 +87,7 @@ server <- function(input, output) {
     paste0(
       "Country: ", data$key, "\n", "CO2 emission: ",
       round(data$y, digits = 2), "\n",
-      "GDP per capital: ", round(data$x, digits = 2)
+      "GDP per capita: ", round(data$x, digits = 2)
     )
   })
 }
