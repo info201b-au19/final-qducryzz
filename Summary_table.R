25627685db6414d2654f6cec022aac6e247f7e7f
hongyi <hongyi@Stevens-MacBook-Pro.local>
2019-11-18 20:43:38 -0800

    hah

--- Summary_table.R
+++ Summary_table.R
@@ -2,16 +2,16 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 
-# This table talks about the growth rate of population 
+# This table talks about the growth rate of population
 # lifetime, and CO2 emission for all the countries in
-# the world from 1949 to 2014. WIth sum of the all diff,
+# the world from 1949 to 2014. With sum of the all diff,
 # we get overall change for 1949 to 2014. Divided by the time
 # interval, we get the the rate of change of Population, life_expectancy,
 # and CO2 emission from 1949 to 2014.
 # Important info:
-# 1. Based on the table, it is clear to see that the population 
+# 1. Based on the table, it is clear to see that the population
 # change rate is directly proportional to the CO2 emission rate.
-# 2. Compare to two other colomns(population change rate and
+# 2. Compare to two other columns(population change rate and
 # CO2 emission change rate), the lifetime doesn't change that
 # much with a less than one year increase each year from
 # 1949 to 2014.
